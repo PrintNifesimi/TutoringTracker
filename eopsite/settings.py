64d9d6b2d48c51a37ecc322fe979d4a30da1aba3
Nifesimi <nifeakin@icloud.com>
2022-05-30 17:48:14 -0500

    settings update

--- eopsite/settings.py
+++ eopsite/settings.py
@@ -25,10 +25,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('SECRET_KEY')
+SECRET_KEY = "django-insecure-pq!ndtmqm8wg4vbzxq5=nv+^$g-&m64v3wer7%+0b4$qe8!gfa"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config('DEBUG', cast=bool)
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
