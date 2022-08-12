6e3c7fa4004be31d613052577c6141cb95bd0089
Nifesimi <96543196+PrintNifesimi@users.noreply.github.com>
2022-08-11 19:34:16 -0500

    Updated default page

--- main/urls.py
+++ main/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index"),
+    path("", views.loginPage, name="loginPage"),
     path("login/",views.loginPage, name="loginPage"),
     path("logout/", views.logoutUser, name="logout"),
     path("NewSession", views.Sessions, name="Sessions"),
